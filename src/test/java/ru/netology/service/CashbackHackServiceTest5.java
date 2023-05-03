95d14d0661eee6fe0a302fee3e9e3ec3599f0bf8
s_ishukov <s_ishukov@bk.ru>
2023-05-03 14:17:10 +0500

    JUnit5 Tests add

--- src/test/java/ru/netology/service/CashbackHackServiceTest5.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest5.java
@@ -1,19 +1,18 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
-
-public class CashbackHackServiceTest {
+class CashbackHackServiceTest5 {
     @Test
     public void shouldCashBackServiceWithPurchase0(){
         CashbackHackService service = new CashbackHackService();
         int expected = service.remain(0);
         int actual = 1000;
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected,actual);
     }
     @Test
     public void shouldCashBackServiceWithPurchase500(){
@@ -21,7 +20,7 @@ public class CashbackHackServiceTest {
         int expected = service.remain(500);
         int actual = 500;
 
-        Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
     @Test
     public void shouldCashBackServiceWithPurchase999(){
@@ -29,7 +28,7 @@ public class CashbackHackServiceTest {
         int expected = service.remain(999);
         int actual = 1;
 
-        Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
     @Test
     public void shouldCashBackServiceWithPurchase1000(){
@@ -37,7 +36,7 @@ public class CashbackHackServiceTest {
         int expected = service.remain(1000);
         int actual = 0;
 
-        Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
     @Test
     public void shouldCashBackServiceWithPurchase1001(){
@@ -45,6 +44,6 @@ public class CashbackHackServiceTest {
         int expected = service.remain(1001);
         int actual = 999;
 
-        Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
 }
